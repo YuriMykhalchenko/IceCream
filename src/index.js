d7a6b295497ecfb1a6d03a20db79775b38cc523f
YuriMykhalchenko <108886900+YuriMykhalchenko@users.noreply.github.com>
2023-03-30 19:12:05 +0200

    update

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './js/franchise-modal';
+import './js/mobile-menu';
+import './js/locations-modal';
+import './js/read-more-modal';
+import './js/buy-now-modal';
+import './js/swiper';
+import './js/slider';
+// import './js/modal-1';
+import './js/modal-ingredients';
